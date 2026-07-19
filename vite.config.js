95612a70fc09fe3bac91e8384e33988c31f54313
ireflux <mapleblue2007@gmail.com>
2026-07-19 23:05:14 +0800

    fix path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/JSON-Formatter/',
+  base: process.env.BASE_PATH || '/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
